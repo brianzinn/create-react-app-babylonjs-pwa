04776f5c8428f163c47212df418f174d34ec9514
Brian Zinn <github@wakeskate.com>
2019-01-19 16:52:34 -0800

    BabylonJS PWA v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Engine, Scene, HemisphericLight, ArcRotateCamera, Skybox } from 'react-babylonjs'
+import ScaledModelWithProgress from './ScaledModelWithProgress'
 import './App.css';
+import { Vector3, Color3  } from 'babylonjs';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Engine antialias={true} adaptToDeviceRatio={true} canvasId="game-canvas">
+          <Scene>
+            <ArcRotateCamera name="camera" alpha={Math.PI / 2} beta={Math.PI / 3}
+              radius={3} target={Vector3.Zero()} minZ={0.001}
+            />
+            
+            <HemisphericLight name="light" direction={Vector3.Up()} />
+            <ScaledModelWithProgress
+              rootUrl={`${process.env.PUBLIC_URL}/assets/glTF/`} sceneFilename="BoomBox.gltf"
+              scaleTo={1} 
+              progressBarColor={Color3.FromInts(255, 165, 0)} center={new Vector3(0, -0.2, 0)}
+            />
+            
+            <Skybox rootUrl={`${process.env.PUBLIC_URL}/assets/skybox/TropicalSunnyDay`} />
+          </Scene>
+        </Engine>
       </div>
     );
   }
